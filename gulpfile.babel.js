e36438cdbf6264f558d439cfaaa77a1a530cf977
Aaron Triantafyllidis and Carlo Francisco <pair+azzamallow+carlo.francisco@refinery29.com>
2016-03-11 14:21:13 -0500

    Strip out babel

--- gulpfile.babel.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import { Server as KarmaServer } from 'karma';
-
-gulp.task('test', (cb) => {
-  new KarmaServer({
-    configFile: `${__dirname}/karma.conf.js`
-  }, cb).start();
-});
